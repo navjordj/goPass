4793dbf4c087953d6eef4a045441da84c057595d
navjordj <navjordj@gmail.com>
2020-11-17 13:27:26 +0100

    Check if website is in DB

---
+++ database/misc.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+)
+
+func CheckInDatabase(website string, db *sql.DB) bool {
+	statement, _ := db.Prepare("SELECT website FROM passwords")
+	rows, _ := statement.Query()
+
+	var websiteRes string
+
+	for rows.Next() {
+		rows.Scan(&websiteRes)
+
+		if websiteRes == website {
+			return true
+		}
+	}
+	return false
+}
